3fb0f485f670f387ea31df22691bec3686a450d9
tatsuru koike <t-koike@marsflag.com>
2025-01-08 22:31:41 +0900

    refactor

--- frontend/src/app/todo/page.tsx
+++ frontend/src/app/todo/page.tsx
@@ -1,148 +1,19 @@
-"use client";
-
-import { useState, useEffect } from "react";
-import { Input } from "@/components/shadcn-ui/input";
-import { Button } from "@/components/shadcn-ui/button";
 import { TodoTable } from "@/components/shared/todoTable";
+import { Suspense } from "react";
 
 interface Todo {
   id: string;
   body: string;
 }
 
-async function getTodos() {
-  try {
-    const response = await fetch("http://localhost:8000/items", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      cache: "no-store",
-      next: { revalidate: 0 },
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Failed to fetch todos:", error);
-    return [];
-  }
-}
-
-async function createTodo(body: string) {
-  try {
-    const response = await fetch("http://localhost:8000/items", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ body }),
-      cache: "no-store",
-      next: { revalidate: 0 },
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Failed to create todo:", error);
-  }
-}
-
-async function deleteTodo(id: string) {
-  try {
-    const response = await fetch(`http://localhost:8000/items/${id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      cache: "no-store",
-      next: { revalidate: 0 },
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    console.log(`Success to delete todo item: ${id}`);
-    return data;
-  } catch (error) {
-    console.error("Failed to delete todo:", error);
-  }
-}
-
 export default function Todo() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [inputContent, setInputContent] = useState("");
-
-  useEffect(() => {
-    async function fetchData() {
-      const todoData = await getTodos();
-      setTodos(todoData);
-    }
-    fetchData();
-  }, []);
-
-  const handleDeleteButtonClick = async (todoId: string) => {
-    await deleteTodo(todoId);
-
-    async function fetchData() {
-      const todoData = await getTodos();
-      setTodos(todoData);
-    }
-    fetchData();
-  };
-
-  const handleCreateButtonClick = async (body: string) => {
-    await createTodo(body);
-
-    async function fetchData() {
-      const todoData = await getTodos();
-      setTodos(todoData);
-    }
-
-    fetchData();
-    setInputContent("");
-  };
-
   return (
     <div className="h-screen bg-muted-foreground p-8">
       <div className="flex flex-col gap-8 h-full bg-white rounded-lg p-8">
         <h1 className="text-xl font-semibold">TODO APP</h1>
-        <div className="flex gap-4">
-          <Input
-            value={inputContent}
-            onChange={(e) => {
-              setInputContent(e.target.value);
-            }}
-            placeholder="やることを入力"
-          />
-          <Button
-            onClick={() => {
-              handleCreateButtonClick(inputContent);
-            }}
-            disabled={inputContent === ""}
-          >
-            登録
-          </Button>
-        </div>
-
-        {todos.length === 0 && (
-          <div>
-            <p className="text-sm">やることがありません</p>
-          </div>
-        )}
-        {todos.length > 0 && (
-          <TodoTable todos={todos} onButtonClick={handleDeleteButtonClick} />
-        )}
+        <Suspense fallback={<div>Loading...</div>}>
+          <TodoTable />
+        </Suspense>
       </div>
     </div>
   );
--- frontend/src/components/shared/todoTable.tsx
+++ frontend/src/components/shared/todoTable.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import { Button } from "@/components/shadcn-ui/button";
+import { Input } from "@/components/shadcn-ui/input";
 import {
   Table,
   TableBody,
@@ -7,45 +11,145 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/shadcn-ui/table";
-// import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface Todo {
   id: string;
   body: string;
 }
 
-interface TodoTableProps {
-  todos: Todo[];
-  onButtonClick: (todoId: string) => void;
+async function getTodos() {
+  try {
+    const response = await fetch("http://localhost:8000/items", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      cache: "no-store",
+      next: { revalidate: 0 },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Failed to fetch todos:", error);
+    return [];
+  }
 }
 
-export function TodoTable({ todos, onButtonClick }: TodoTableProps) {
+async function createTodo(body: string) {
+  try {
+    const response = await fetch("http://localhost:8000/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ body }),
+      cache: "no-store",
+      credentials: "include",
+      next: { revalidate: 0 },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Failed to create todo:", error);
+  }
+}
+
+async function deleteTodo(id: string) {
+  try {
+    const response = await fetch(`http://localhost:8000/items/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cache: "no-store",
+      credentials: "include",
+      next: { revalidate: 0 },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(`Success to delete todo item: ${id}`);
+    return data;
+  } catch (error) {
+    console.error("Failed to delete todo:", error);
+  }
+}
+
+export function TodoTable() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    getTodos().then((data) => {
+      setTodos(data);
+    });
+  }, []);
+
+  const handleCreate = async () => {
+    await createTodo(inputValue);
+    getTodos().then((data) => {
+      setTodos(data);
+    });
+  };
+
+  const handleDelete = async (id: string) => {
+    await deleteTodo(id);
+    getTodos().then((data) => {
+      setTodos(data);
+    });
+  };
+
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-96">ID</TableHead>
-          <TableHead>TODO</TableHead>
-          <TableHead className="text-right">Menu</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {todos.map((todo) => (
-          <TableRow key={todo.id}>
-            <TableCell>{todo.id}</TableCell>
-            <TableCell>{todo.body}</TableCell>
-            <TableCell className="text-right">
-              <Button
-                variant="destructive"
-                size="sm"
-                onClick={() => onButtonClick(todo.id)}
-              >
-                delete
-              </Button>
-            </TableCell>
+    <>
+      <div className="flex gap-4">
+        <Input
+          placeholder="やることを入力"
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <Button variant="default" onClick={handleCreate}>
+          登録
+        </Button>
+      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-96">ID</TableHead>
+            <TableHead>TODO</TableHead>
+            <TableHead className="text-right">Menu</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {todos.map((todo) => (
+            <TableRow key={todo.id}>
+              <TableCell>{todo.id}</TableCell>
+              <TableCell>{todo.body}</TableCell>
+              <TableCell className="text-right">
+                <Button
+                  variant="destructive"
+                  size="sm"
+                  onClick={() => handleDelete(todo.id)}
+                >
+                  delete
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
   );
 }
