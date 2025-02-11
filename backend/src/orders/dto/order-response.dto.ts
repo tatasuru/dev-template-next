81ce9ebda7817f61412ce7824e709619900dd3e8
iromonek39 <kentarou.omori0309@gmail.com>
2025-02-11 17:50:07 +0900

    orders: add OrderCreateDto for order response structure

---
+++ backend/src/orders/dto/order-response.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class OrderCreateDto {
+  @ApiProperty({
+    example: 0o12,
+    description: 'オーダーID',
+  })
+  order_id: number;
+
+  @ApiProperty({
+    example: '注文の詳細',
+    description: '注文詳細',
+  })
+  order_details: string;
+}
